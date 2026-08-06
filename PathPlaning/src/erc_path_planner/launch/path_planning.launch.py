9afdc61e8007df46f310422d491fc5f61b8f3f22
abdo-khallaf <abdo10820004@gmail.com>
2026-08-06 16:11:37 +0300

    Task A-6.1: Include nav2_bringup with custom params

--- PathPlaning/src/erc_path_planner/launch/path_planning.launch.py
+++ PathPlaning/src/erc_path_planner/launch/path_planning.launch.py
@@ -1,5 +1,37 @@
 from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+import os
 
 
 def generate_launch_description():
-    return LaunchDescription([])
+
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    erc_path_planner_dir = get_package_share_directory('erc_path_planner')
+
+    params_file = os.path.join(
+        erc_path_planner_dir,
+        'config',
+        'nav2_params.yaml'
+    )
+
+    nav2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                nav2_bringup_dir,
+                'launch',
+                'bringup_launch.py'
+            )
+        ),
+        launch_arguments={
+            'params_file': params_file,
+            'use_sim_time': 'false'
+        }.items()
+    )
+
+    return LaunchDescription([
+        nav2_launch
+    ])
